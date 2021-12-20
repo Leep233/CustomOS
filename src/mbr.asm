43787fbe1d7dc7aec70a0e02b919bb22db5fc3bd
邓宇鹏 <454199506@qq.com>
2021-12-20 18:02:11 +0800

    添加清屏

--- src/mbr.asm
+++ src/mbr.asm
@@ -1,16 +1,28 @@
 
 section mbr vstart=0x7c00
 
+;clear display
+   mov ax,0x0600
+    mov bx,0x0700
+    mov cx,0
+    mov dx,0x184f
+    int 0x10
+
     mov si,0x500
     mov cx,1
     mov eax,1
 
- 
     jmp read_disk_16
 
     jmp 0x0000:0x500
 
+    ;jmp $
+   
      %include ".\src\disk_reader_16.asm"
+
+
+    
+ 
 
 times 510-($-$$) db 0
 db 0x55,0xaa
